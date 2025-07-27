35df1c5d9b5923b9ddd5483fe2b7a82c97056be6
Atul Kumar Gupta <atulgupta3058@gmail.com>
2025-07-27 23:11:27 +0530

    minor bugs fixed

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,4 @@
-import app from "../server/app.js";
+import app from "../app.js";
 import serverless from "serverless-http";
 
-export const handler = serverless(app);
+export default serverless(app);
